80030edaa84a611c3fe0732bf968a53d8147b810
Wesley Soo-Hoo <wsoohoo32@gmail.com>
2015-05-10 15:18:29 -0700

    Moved function out of jQuery control

--- js/ctf.js
+++ js/ctf.js
@@ -1,30 +1,31 @@
 $(document).ready(function(){
 	//initialize code here
 	
-	function update_ranking()
-	{
-		$.get
-		(
-			'ajax.php?m=get_ranking',
-			function( xml )
-			{
-				var table = $( '<table>' );
-
-				$( xml ).find( 'team' ).each
-				(
-					function()
-					{
-						var trow = $( '<tr>' );
-						$( '<td>' ).text( $( this ).find( 'rank' ).text() ).appendTo( trow );
-						$( '<td>' ).text( $( this ).find( 'name' ).text() ).appendTo( trow );
-						$( '<td>' ).addClass( 'score' ).text( $( this ).find( 'score' ).text() ).appendTo( trow );
-
-						trow.appendTo( table );
-					}
-				);
-
-				$( '#ranking' ).html( table.html() );
-			}
-		);
-	}
 });
+
+function update_ranking()
+{
+	$.get
+	(
+		'ajax.php?m=get_ranking',
+		function( xml )
+		{
+			var table = $( '<table>' );
+
+			$( xml ).find( 'team' ).each
+			(
+				function()
+				{
+					var trow = $( '<tr>' );
+					$( '<td>' ).text( $( this ).find( 'rank' ).text() ).appendTo( trow );
+					$( '<td>' ).text( $( this ).find( 'name' ).text() ).appendTo( trow );
+					$( '<td>' ).addClass( 'score' ).text( $( this ).find( 'score' ).text() ).appendTo( trow );
+
+					trow.appendTo( table );
+				}
+			);
+
+			$( '#ranking' ).html( table.html() );
+		}
+	);
+}
